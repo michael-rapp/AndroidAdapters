030b2cb858bdd98289662514cc87d8044bc5579c
mrapp <mrapp@localhost>
2014-05-08 11:22:46 +0200

    Added the interface ObservableListAdapter.

---
+++ src/de/mrapp/android/adapter/list/ObservableListAdapter.java
@@ -0,0 +1,55 @@
+/*
+ * AndroidAdapters Copyright 2014 Michael Rapp
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>. 
+ */
+package de.mrapp.android.adapter.list;
+
+/**
+ * Defines the interface, all classes, which should allow to register listeners,
+ * which are notified when the underlying data of a {@link ListAdapter} has been
+ * modified, must implement.
+ * 
+ * @param <ItemType>
+ *            The type of the adapter's underlying data
+ * 
+ * @author Michael Rapp
+ * 
+ * @since 1.0.0
+ */
+public interface ObservableListAdapter<ItemType> {
+
+	/**
+	 * Adds a new listener, which should be notified when the adapter's
+	 * underlying data has been modified.
+	 * 
+	 * @param listener
+	 *            The listener, which should be added, as an instance of the
+	 *            class {@link ListAdapterListener}. The listener may not be
+	 *            null
+	 */
+	void addAdapterListener(ListAdapterListener<ItemType> listener);
+
+	/**
+	 * Removes a specific listener, which should not be notified when the
+	 * adapter's underlying data has been modified, anymore.
+	 * 
+	 * @param listener
+	 *            The listener, which should be removed, as an instance of the
+	 *            class {@link ListAdapterListener}. The listener may not be
+	 *            null
+	 */
+	void removeAdapterListener(ListAdapterListener<ItemType> listener);
+
+}
