1b4f6a0497c964a59058bf7ddd588dd3c4966b5b
Michael Rapp <michael.rapp90@googlemail.com>
2014-05-08 11:03:58 +0200

    Moved interface ListAdapter into parent package.

--- src/de/mrapp/android/adapter/ListAdapter.java
+++ src/de/mrapp/android/adapter/ListAdapter.java
@@ -14,11 +14,12 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>. 
  */
-package de.mrapp.android.adapter.list;
+package de.mrapp.android.adapter;
 
 import java.util.List;
 
 import android.content.Context;
+import de.mrapp.android.adapter.list.ListAdapterListener;
 import de.mrapp.android.adapter.list.selection.ListSelectionListener;
 
 /**
