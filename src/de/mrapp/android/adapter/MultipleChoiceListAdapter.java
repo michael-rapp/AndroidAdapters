108d5790b1da2fe50996b3fce89140f69c5ce6de
Michael Rapp <michael.rapp90@googlemail.com>
2014-05-15 23:42:59 +0200

    Added the interface MultipleChoiceListAdapter.

---
+++ src/de/mrapp/android/adapter/MultipleChoiceListAdapter.java
@@ -0,0 +1,40 @@
+/*
+ * AndroidAdapters Copyright 2014 Michael Rapp
+ *
+ * This program is free software: you can redistribute it and/or modify 
+ * it under the terms of the GNU Lesser General Public License as published 
+ * by the Free Software Foundation, either version 3 of the License, or 
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but 
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU 
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public 
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/>. 
+ */
+package de.mrapp.android.adapter;
+
+/**
+ * Defines the interface, an adapter, whose underlying data is managed as a list
+ * of arbitrary items of which multiple items can be selected at once, must
+ * implement. Such an adapter is meant to provide the underlying data for
+ * visualization using a {@link ListView} widget.
+ * 
+ * @param <DataType>
+ *            The type of the adapter's underlying data
+ * 
+ * @author Michael Rapp
+ * 
+ * @since 1.0.0
+ */
+public interface MultipleChoiceListAdapter<DataType> extends
+		ListAdapter<DataType> {
+
+	@Override
+	MultipleChoiceListAdapter<DataType> clone()
+			throws CloneNotSupportedException;
+
+}
