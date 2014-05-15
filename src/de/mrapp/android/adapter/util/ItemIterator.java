a2181b44222038f39c74ed8edfa7ef1343904334
mrapp <mrapp@localhost>
2014-05-16 01:22:56 +0200

    Added the class ItemIterator.

---
+++ src/de/mrapp/android/adapter/util/ItemIterator.java
@@ -0,0 +1,66 @@
+/*
+ * AndroidAdapters Copyright 2014 Michael Rapp
+ *
+ * This program is free software: you can redistribute it and/or modify 
+ * it under the terms of the GNU Lesser General Public License as published 
+ * by the Free Software Foundation, either version 3 of the License, or 
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but 
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU 
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public 
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/>. 
+ */
+package de.mrapp.android.adapter.util;
+
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ * An iterator, which allows to iterate over the data of a list's items.
+ * 
+ * @param <DataType>
+ *            The type of the item's data
+ * 
+ * @author Michael Rapp
+ */
+public class ItemIterator<DataType> implements Iterator<DataType> {
+
+	/**
+	 * A list, which contains the items, whose data should be iterated.
+	 */
+	private List<Item<DataType>> items;
+
+	/**
+	 * Creates a new iterator, which allows to iterate over the data of a list's
+	 * items.
+	 * 
+	 * @param items
+	 *            The list, which contains the items, whose data should be
+	 *            iterated, as an instance of the type {@link List}. The list
+	 *            may not be null
+	 */
+	public ItemIterator(final List<Item<DataType>> items) {
+		this.items = items;
+	}
+
+	@Override
+	public final boolean hasNext() {
+		return items.iterator().hasNext();
+	}
+
+	@Override
+	public final DataType next() {
+		return items.iterator().next().getData();
+	}
+
+	@Override
+	public final void remove() {
+		items.iterator().remove();
+	}
+
+}
