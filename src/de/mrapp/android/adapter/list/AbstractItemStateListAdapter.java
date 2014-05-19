9245dc998f2bd922f0ff91318ea0ea2470c458e1
mrapp <mrapp@localhost>
2014-05-20 00:45:35 +0200

    Added methods, which allow to modify/retrieve the states of specific items.

--- src/de/mrapp/android/adapter/list/AbstractItemStateListAdapter.java
+++ src/de/mrapp/android/adapter/list/AbstractItemStateListAdapter.java
@@ -17,14 +17,16 @@
  */
 package de.mrapp.android.adapter.list;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
 import android.content.Context;
 import android.view.View;
 import de.mrapp.android.adapter.util.Item;
-
-import static de.mrapp.android.adapter.util.Condition.ensureGreaterThan;
+import static de.mrapp.android.adapter.util.Condition.ensureAtLeast;
+import static de.mrapp.android.adapter.util.Condition.ensureAtMaximum;
+import static de.mrapp.android.adapter.util.Condition.ensureNotNull;
 
 /**
  * An abstract base class for all adapters, whose underlying data is managed as
@@ -52,6 +54,65 @@ public abstract class AbstractItemStateListAdapter<DataType> extends
 	 */
 	private int numberOfItemStates;
 
+	/**
+	 * A set, which contains the listeners, which should be notified, when the
+	 * state of an item of the adapter has been changed.
+	 */
+	private Set<ListItemStateListener<DataType>> itemStateListeners;
+
+	/**
+	 * Notifies all listeners, which have registered to be notified, when the
+	 * state of an item of the adapter has been changed.
+	 * 
+	 * @param item
+	 *            The item, whose state has been changed, as an instance of the
+	 *            generic type DataType. The item may not be null
+	 * @param index
+	 *            The index of the item, whose state has been changed, as an
+	 *            {@link Integer} value. The index must be between 0 and the
+	 *            value of the adapter's <code>size():int</code> method - 1
+	 * @param state
+	 *            The new state of the item, whose state has been changed, as an
+	 *            {@link Integer} value. The state must be at least 0 and less
+	 *            than the value of the method
+	 *            <code>getNumberOfStates():int</code> - 1
+	 */
+	private void notifyOnItemStateChanged(final DataType item, final int index,
+			final int state) {
+		for (ListItemStateListener<DataType> listener : itemStateListeners) {
+			listener.onItemStateChanged(item, index, state);
+		}
+	}
+
+	/**
+	 * Returns the set, which contains the listeners, which should be notified,
+	 * when the state of an item of the adapter has been changed.
+	 * 
+	 * @return The set, which contains the listeners, which should be notified,
+	 *         when the state of an item of the adapter has been changed, as an
+	 *         instance of the type {@link Set} or an empty set, if no listeners
+	 *         should be notified
+	 */
+	protected final Set<ListItemStateListener<DataType>> getItemStateListeners() {
+		return itemStateListeners;
+	}
+
+	/**
+	 * Sets the set, which contains the listeners, which should be notified,
+	 * when the state of an item of the adapter has been changed.
+	 * 
+	 * @param itemStateListeners
+	 *            The set, which should be set, as an instance of the type
+	 *            {@link Set} or an empty set, if no listeners should be
+	 *            notified
+	 */
+	protected final void setItemStateListeners(
+			final Set<ListItemStateListener<DataType>> itemStateListeners) {
+		ensureNotNull(itemStateListeners,
+				"The item state listeners may not be null");
+		this.itemStateListeners = itemStateListeners;
+	}
+
 	protected AbstractItemStateListAdapter(final Context context,
 			final int itemViewId, final View itemView,
 			final List<Item<DataType>> items,
@@ -61,8 +122,8 @@ public abstract class AbstractItemStateListAdapter<DataType> extends
 			final int numberOfItemStates) {
 		super(context, itemViewId, itemView, items, adapterListeners,
 				enableStateListeners, sortingListeners);
-		ensureGreaterThan(numberOfItemStates, 0, "The number of items states "
-				+ "must be greater than 0");
+		ensureAtLeast(numberOfItemStates, 1, "The number of items states "
+				+ "must be at least 1");
 		this.numberOfItemStates = numberOfItemStates;
 	}
 
@@ -76,8 +137,139 @@ public abstract class AbstractItemStateListAdapter<DataType> extends
 		return numberOfItemStates;
 	}
 
+	/**
+	 * Returns the state of the item, which belongs to a specific index.
+	 * 
+	 * @param index
+	 *            The index of the item, whose state should be returned, as an
+	 *            {@link Integer} value. The index must be between 0 and the
+	 *            value of the adapter's <code>size():int</code> method - 1
+	 * @return The state of the item, which belongs to the given index, as a
+	 *         {@link Integer} value. The state must be at least 0 and less than
+	 *         the value of the method <code>getNumberOfStates():int</code> - 1
+	 */
 	public final int getItemState(final int index) {
-		return getItems().get(index).ge
+		return getItems().get(index).getState();
+	}
+
+	public final int getItemState(final DataType item) {
+		return getItemState(indexOf(item));
+	}
+
+	public final int setItemState(final int index, final int state) {
+		ensureAtMaximum(state, numberOfItemStates - 1,
+				"The state may be at maximum " + (numberOfItemStates - 1));
+		Item<DataType> item = getItems().get(index);
+		int previousState = item.getState();
+		item.setState(state);
+		notifyOnItemStateChanged(item.getData(), index, state);
+		notifyDataSetInvalidated();
+		return previousState;
+	}
+
+	public final void setAllItemStates(final int state) {
+		for (int i = 0; i < size(); i++) {
+			setItemState(i, state);
+		}
+	}
+
+	public final int triggerItemState(final int index) {
+		Item<DataType> item = getItems().get(index);
+		int previousState = item.getState();
+
+		if (previousState == numberOfItemStates - 1) {
+			item.setState(0);
+		} else {
+			item.setState(previousState + 1);
+		}
+
+		notifyOnItemStateChanged(item.getData(), index, item.getState());
+		notifyDataSetInvalidated();
+		return previousState;
+	}
+
+	public final int triggerItemState(final DataType item) {
+		return triggerItemState(indexOf(item));
+	}
+
+	public final void triggerAllItemStates() {
+		for (int i = 0; i < size(); i++) {
+			triggerItemState(i);
+		}
+	}
+
+	public final int setItemState(final DataType item, final int state) {
+		return setItemState(indexOf(item), state);
+	}
+
+	public final int getFirstIndexWithSpecificState(final int state) {
+		for (int i = 0; i < size(); i++) {
+			if (getItems().get(i).getState() == state) {
+				return i;
+			}
+		}
+
+		return -1;
+	}
+
+	public final DataType getFirstItemWithSpecificState(final int state) {
+		for (Item<DataType> item : getItems()) {
+			if (item.getState() == state) {
+				return item.getData();
+			}
+		}
+
+		return null;
+	}
+
+	public final int getLastIndexWithSpecificState(final int state) {
+		for (int i = size() - 1; i >= 0; i--) {
+			if (getItems().get(i).getState() == state) {
+				return i;
+			}
+		}
+
+		return -1;
+	}
+
+	public final DataType getLastItemWithSpecificState(final int state) {
+		for (int i = size() - 1; i >= 0; i--) {
+			Item<DataType> item = getItems().get(i);
+
+			if (item.getState() == state) {
+				return item.getData();
+			}
+		}
+
+		return null;
+	}
+
+	public final List<Integer> getIndicesWithSpecificState(final int state) {
+		List<Integer> indices = new ArrayList<Integer>();
+
+		for (int i = 0; i < size(); i++) {
+			if (getItems().get(i).getState() == state) {
+				indices.add(i);
+			}
+		}
+
+		return indices;
+	}
+
+	public final List<DataType> getItemsWithSpecificState(final int state) {
+		List<DataType> items = new ArrayList<DataType>();
+
+		for (Item<DataType> item : getItems()) {
+			if (item.getState() == state) {
+				items.add(item.getData());
+			}
+		}
+
+		return items;
+	}
+
+	public final int getNumberOfItemsWithSpecificState(final int state) {
+		return getItemsWithSpecificState(state).size();
 	}
 
 	@Override
