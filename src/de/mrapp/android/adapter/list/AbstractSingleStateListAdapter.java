9c122a7aa3342620cbf61da617618f84331a3669
Michael Rapp <michael.rapp90@googlemail.com>
2014-05-19 23:28:21 +0200

    Changed return types of clone methods.

--- src/de/mrapp/android/adapter/list/AbstractSingleStateListAdapter.java
+++ src/de/mrapp/android/adapter/list/AbstractSingleStateListAdapter.java
@@ -147,7 +147,7 @@ public abstract class AbstractSingleStateListAdapter<DataType> extends
 	}
 
 	@Override
-	public abstract AbstractListAdapter<DataType> clone()
+	public abstract AbstractSingleStateListAdapter<DataType> clone()
 			throws CloneNotSupportedException;
 
 }
