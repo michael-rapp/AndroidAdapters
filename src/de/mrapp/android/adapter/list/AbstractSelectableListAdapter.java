7e1b94b08304cca81c1735c6ed252794d16461ec
mrapp <mrapp@localhost>
2014-05-23 01:55:39 +0200

    Added the interface SelectableListAdapter.

--- src/de/mrapp/android/adapter/list/AbstractSelectableListAdapter.java
+++ src/de/mrapp/android/adapter/list/AbstractSelectableListAdapter.java
@@ -13,7 +13,8 @@ import de.mrapp.android.adapter.util.Item;
 import de.mrapp.android.adapter.util.Logger;
 
 public abstract class AbstractSelectableListAdapter<DataType> extends
-		AbstractItemStateListAdapter<DataType> {
+		AbstractItemStateListAdapter<DataType> implements
+		SelectableListAdapter<DataType> {
 
 	/**
 	 * The constant serial version UID.
@@ -102,61 +103,25 @@ public abstract class AbstractSelectableListAdapter<DataType> extends
 		this.selectionListeners = selectionListeners;
 	}
 
-	/**
-	 * Adds a new listener, which should be notified when the selection of an
-	 * item has been changed.
-	 * 
-	 * @param listener
-	 *            The listener, which should be added, as an instance of the
-	 *            type {@link ListSelectionListener}. The listener may not be
-	 *            null
-	 */
+	@Override
 	public final void addSelectionListener(
 			final ListSelectionListener<DataType> listener) {
 		ensureNotNull(listener, "The listener may not be null");
 		selectionListeners.add(listener);
 	}
 
-	/**
-	 * Removes a specific listener, which should not be notified when the
-	 * selection of an item has been changed, anymore.
-	 * 
-	 * @param listener
-	 *            The listener, which should be removed, as an instance of the
-	 *            type {@link ListSelectionListener}. The listener may not be
-	 *            null
-	 */
+	@Override
 	public final void removeSelectionListener(
 			final ListSelectionListener<DataType> listener) {
 		selectionListeners.remove(listener);
 	}
 
-	/**
-	 * Returns, whether the item, which belongs to a specific index, is
-	 * selected, or not.
-	 * 
-	 * @param index
-	 *            The index of the item, whose selection state should be
-	 *            returned, as an {@link Integer} value. The index must be
-	 *            between 0 and the value of the method
-	 *            <code>getNumberOfItems():int</code> - 1
-	 * @return True, if the item, which belongs to the given index, is selected,
-	 *         false otherwise
-	 */
+	@Override
 	public final boolean isSelected(final int index) {
 		return getItems().get(index).isSelected();
 	}
 
-	/**
-	 * Returns, whether a specific item is selected, or not.
-	 * 
-	 * @param item
-	 *            The item, whose selection state should be returned, as an
-	 *            instance of the generic type DataType. The item may not be
-	 *            null <code>getNumberOfItems():int</code> - 1
-	 * @return True, if the item, which belongs to the given index, is selected,
-	 *         false otherwise
-	 */
+	@Override
 	public final boolean isSelected(final DataType item) {
 		return isSelected(indexOf(item));
 	}
