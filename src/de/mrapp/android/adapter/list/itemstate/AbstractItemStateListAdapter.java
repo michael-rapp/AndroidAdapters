cffb488b82cf1e466bcfb272cbf1e6bd375f8c7f
mrapp <mrapp@localhost>
2014-05-23 01:58:54 +0200

    Created package itemstate.

--- src/de/mrapp/android/adapter/list/itemstate/AbstractItemStateListAdapter.java
+++ src/de/mrapp/android/adapter/list/itemstate/AbstractItemStateListAdapter.java
@@ -15,7 +15,7 @@
  * License along with this program.  If not, see
  * <http://www.gnu.org/licenses/>. 
  */
-package de.mrapp.android.adapter.list;
+package de.mrapp.android.adapter.list.itemstate;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -23,6 +23,7 @@ import java.util.Set;
 
 import android.content.Context;
 import android.view.View;
+import de.mrapp.android.adapter.list.ListAdapterListener;
 import de.mrapp.android.adapter.list.enablestate.ListEnableStateListener;
 import de.mrapp.android.adapter.list.sorting.AbstractSortableListAdapter;
 import de.mrapp.android.adapter.list.sorting.ListSortingListener;
