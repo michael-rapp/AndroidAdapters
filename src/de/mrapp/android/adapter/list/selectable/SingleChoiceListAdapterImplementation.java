0e9c8f5bc22007fde120fbf415fd4840f71e4eff
mrapp <mrapp@localhost>
2014-05-23 02:14:41 +0200

    Added the interface SingleChoiceListAdapter.

--- src/de/mrapp/android/adapter/list/selectable/SingleChoiceListAdapterImplementation.java
+++ src/de/mrapp/android/adapter/list/selectable/SingleChoiceListAdapterImplementation.java
@@ -6,13 +6,15 @@ import java.util.Set;
 import android.content.Context;
 import android.view.View;
 import de.mrapp.android.adapter.SelectableListDecorator;
+import de.mrapp.android.adapter.SingleChoiceListAdapter;
 import de.mrapp.android.adapter.list.ListAdapterListener;
 import de.mrapp.android.adapter.list.enablestate.ListEnableStateListener;
 import de.mrapp.android.adapter.list.sortable.ListSortingListener;
 import de.mrapp.android.adapter.util.Item;
 
 public class SingleChoiceListAdapterImplementation<DataType> extends
-		AbstractSelectableListAdapter<DataType> {
+		AbstractSelectableListAdapter<DataType> implements
+		SingleChoiceListAdapter<DataType> {
 
 	/**
 	 * The constant serial version UID.
@@ -59,14 +61,7 @@ public class SingleChoiceListAdapterImplementation<DataType> extends
 		addAdapterListener(getAdapterListener());
 	}
 
-	/**
-	 * Returns the currently selected index.
-	 * 
-	 * @return The currently selected index, as an {@link Integer} value or -1,
-	 *         if the adapter does not contain any items. The index must be
-	 *         between 0 and the value of the method
-	 *         <code>getNumberOfItems():int</code> - 1
-	 */
+	@Override
 	public final int getSelectedIndex() {
 		for (int i = 0; i < getNumberOfItems(); i++) {
 			if (getItems().get(i).isSelected()) {
@@ -77,12 +72,7 @@ public class SingleChoiceListAdapterImplementation<DataType> extends
 		return -1;
 	}
 
-	/**
-	 * Returns the currently selected item.
-	 * 
-	 * @return The currently selected item, as an instance of the generic type
-	 *         DataType or null, if the adapter does not contain any items
-	 */
+	@Override
 	public final DataType getSelectedItem() {
 		for (Item<DataType> item : getItems()) {
 			if (item.isSelected()) {
@@ -93,15 +83,7 @@ public class SingleChoiceListAdapterImplementation<DataType> extends
 		return null;
 	}
 
-	/**
-	 * Selects the item, which belongs to a specific index. This causes any
-	 * other selected item to become unselected.
-	 * 
-	 * @param index
-	 *            The index of the item, which should be selected, as an
-	 *            {@link Integer} value. The index must be between 0 and the
-	 *            value of the method <code>getNumberOfItems():int</code> - 1
-	 */
+	@Override
 	public final void select(final int index) {
 		for (int i = 0; i < getNumberOfItems(); i++) {
 			Item<DataType> item = getItems().get(i);
@@ -116,14 +98,7 @@ public class SingleChoiceListAdapterImplementation<DataType> extends
 		}
 	}
 
-	/**
-	 * Selects a specific item. This causes any other selected item to become
-	 * unselected.
-	 * 
-	 * @param item
-	 *            The item, which should be selected, as an instance of the
-	 *            generic type DataType. The item may not be null
-	 */
+	@Override
 	public final void select(final DataType item) {
 		select(indexOf(item));
 	}
