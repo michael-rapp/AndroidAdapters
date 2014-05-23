90a3505f5fe9cfcb563bbfccc5497887dce8c6cf
mrapp <mrapp@localhost>
2014-05-23 02:13:00 +0200

    Added comments.

--- src/de/mrapp/android/adapter/list/selectable/SingleChoiceListAdapterImplementation.java
+++ src/de/mrapp/android/adapter/list/selectable/SingleChoiceListAdapterImplementation.java
@@ -59,6 +59,14 @@ public class SingleChoiceListAdapterImplementation<DataType> extends
 		addAdapterListener(getAdapterListener());
 	}
 
+	/**
+	 * Returns the currently selected index.
+	 * 
+	 * @return The currently selected index, as an {@link Integer} value or -1,
+	 *         if the adapter does not contain any items. The index must be
+	 *         between 0 and the value of the method
+	 *         <code>getNumberOfItems():int</code> - 1
+	 */
 	public final int getSelectedIndex() {
 		for (int i = 0; i < getNumberOfItems(); i++) {
 			if (getItems().get(i).isSelected()) {
@@ -69,6 +77,12 @@ public class SingleChoiceListAdapterImplementation<DataType> extends
 		return -1;
 	}
 
+	/**
+	 * Returns the currently selected item.
+	 * 
+	 * @return The currently selected item, as an instance of the generic type
+	 *         DataType or null, if the adapter does not contain any items
+	 */
 	public final DataType getSelectedItem() {
 		for (Item<DataType> item : getItems()) {
 			if (item.isSelected()) {
@@ -79,6 +93,15 @@ public class SingleChoiceListAdapterImplementation<DataType> extends
 		return null;
 	}
 
+	/**
+	 * Selects the item, which belongs to a specific index. This causes any
+	 * other selected item to become unselected.
+	 * 
+	 * @param index
+	 *            The index of the item, which should be selected, as an
+	 *            {@link Integer} value. The index must be between 0 and the
+	 *            value of the method <code>getNumberOfItems():int</code> - 1
+	 */
 	public final void select(final int index) {
 		for (int i = 0; i < getNumberOfItems(); i++) {
 			Item<DataType> item = getItems().get(i);
@@ -93,6 +116,14 @@ public class SingleChoiceListAdapterImplementation<DataType> extends
 		}
 	}
 
+	/**
+	 * Selects a specific item. This causes any other selected item to become
+	 * unselected.
+	 * 
+	 * @param item
+	 *            The item, which should be selected, as an instance of the
+	 *            generic type DataType. The item may not be null
+	 */
 	public final void select(final DataType item) {
 		select(indexOf(item));
 	}
