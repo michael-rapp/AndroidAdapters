20ce75e7f9381194c9c9233e9440103afc430fb9
mrapp <mrapp@localhost>
2014-05-29 17:45:25 +0200

    Edited comments.

--- src/de/mrapp/android/adapter/list/ListAdapterImplementation.java
+++ src/de/mrapp/android/adapter/list/ListAdapterImplementation.java
@@ -1,3 +1,20 @@
+/*
+ * AndroidAdapters Copyright 2014 Michael Rapp
+ *
+ * This program is free software: you can redistribute it and/or modify 
+ * it under the terms of the GNU Lesser General Public License as published 
+ * by the Free Software Foundation, either version 3 of the License, or 
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but 
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU 
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public 
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/>. 
+ */
 package de.mrapp.android.adapter.list;
 
 import static de.mrapp.android.adapter.util.Condition.ensureNotNull;
@@ -15,6 +32,18 @@ import de.mrapp.android.adapter.list.enablestate.ListEnableStateListener;
 import de.mrapp.android.adapter.list.itemstate.AbstractItemStateListAdapter;
 import de.mrapp.android.adapter.list.sortable.ListSortingListener;
 
+/**
+ * An adapter, whose underlying data is managed as a list of arbitrary items.
+ * Such an adapter's purpose is to provide the underlying data for visualization
+ * using a {@link ListView} widget.
+ * 
+ * @param <DataType>
+ *            The type of the adapter's underlying data
+ * 
+ * @author Michael Rapp
+ * 
+ * @since 1.0.0
+ */
 public class ListAdapterImplementation<DataType> extends
 		AbstractItemStateListAdapter<DataType> {
 
@@ -24,12 +53,39 @@ public class ListAdapterImplementation<DataType> extends
 	private static final long serialVersionUID = 1L;
 
 	/**
-	 * The decorator, which is used to customize the appearance of the widgets,
-	 * which belong to the view, which is used to visualize the items of the
-	 * adapter.
+	 * The decorator, which is used to customize the appearance of the views,
+	 * which are used to visualize the items of the adapter.
 	 */
 	private final transient ListDecorator<DataType> decorator;
 
+	/**
+	 * Creates a new adapter, whose underlying data is managed as a list of
+	 * arbitrary items.
+	 * 
+	 * @param context
+	 *            The context, the adapter belongs to, as an instance of the
+	 *            class {@link Context}. The context may not be null
+	 * @param inflater
+	 *            The inflater, which should be used to inflate the views, which
+	 *            are used to visualize the adapter's items, as an instance of
+	 *            the type {@link Inflater}. The inflater may not be null
+	 * @param items
+	 *            A list, which contains the the adapter's underlying data, as
+	 *            an instance of the type {@link List} or an empty list, if the
+	 *            adapter should not contain any data
+	 * @param allowDuplicates
+	 *            True, if duplicate items should be allowed, false otherwise
+	 * @param adapterListeners
+	 *            A set, which contains the listeners, which should be notified,
+	 *            when the adapter's underlying data has been modified, as an
+	 *            instance of the type {@link Set} or an empty set, if no
+	 *            listeners should be notified
+	 * @param decorator
+	 *            The decorator, which should be used to customize the
+	 *            appearance of the views, which are used to visualize the items
+	 *            of the adapter, as an instance of the type
+	 *            {@link ListDecorator}. The decorator may not be null
+	 */
 	protected ListAdapterImplementation(final Context context,
 			final Inflater inflater, final List<Item<DataType>> items,
 			final boolean allowDuplicates,
