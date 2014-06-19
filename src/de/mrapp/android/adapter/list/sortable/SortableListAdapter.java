db0045f80a959f4e8730ca1e3e0323214cb61905
mrapp <mrapp@localhost>
2014-06-19 19:58:34 +0200

    Edited comments.

--- src/de/mrapp/android/adapter/list/sortable/SortableListAdapter.java
+++ src/de/mrapp/android/adapter/list/sortable/SortableListAdapter.java
@@ -1,16 +1,46 @@
+/*
+ * AndroidAdapters Copyright 2014 Michael Rapp
+ *
+ * This program is free software: you can redistribute it and/or modify 
+ * it under the terms of the GNU Lesser General Public License as published 
+ * by the Free Software Foundation, either version 3 of the License, or 
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but 
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU 
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public 
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/>. 
+ */
 package de.mrapp.android.adapter.list.sortable;
 
 import java.util.Comparator;
 
+/**
+ * Defines the interface, an adapter, whose underlying data is managed as a
+ * sortable list of arbitrary items, must implement. Such an adapter's purpose
+ * is to provide the underlying data for visualization using a {@link ListView}
+ * widget.
+ * 
+ * @param <DataType>
+ *            The type of the adapter's underlying data
+ * 
+ * @author Michael Rapp
+ * 
+ * @since 1.0.0
+ */
 public interface SortableListAdapter<DataType> {
 
 	/**
-	 * Sorts the items of the adapter in an ascending order.
+	 * Sorts the adapter's items in an ascending order.
 	 */
 	void sort();
 
 	/**
-	 * Sorts the items of the adapter in a specific order.
+	 * Sorts the adapter's items in a specific order.
 	 * 
 	 * @param order
 	 *            The order, which should be used to sort the items, as a value
@@ -20,33 +50,31 @@ public interface SortableListAdapter<DataType> {
 	void sort(Order order);
 
 	/**
-	 * Sorts the items of the adapter by using a comparator in an ascending
-	 * order.
+	 * Sorts the adapter's items in an ascending order, by using a comparator.
 	 * 
 	 * @param comparator
-	 *            The comparable, which should be used to sort the items, as an
+	 *            The comparator, which should be used to sort the items, as an
 	 *            instance of the type {@link Comparator}. The comparator may
 	 *            not be null
 	 */
 	void sort(Comparator<DataType> comparator);
 
 	/**
-	 * Sorts the entries of the adapter by using a comparator in a specific
-	 * order.
+	 * Sorts the adapter's items in a specific order, by using a comparator.
 	 * 
 	 * @param comparator
-	 *            The comparable, which should be used to sort the entries, as
-	 *            an instance of the type {@link Comparator}. The comparator may
+	 *            The comparator, which should be used to sort the items, as an
+	 *            instance of the type {@link Comparator}. The comparator may
 	 *            not be null
 	 * @param order
-	 *            The order, which should be used to sort the entries, as a
-	 *            value of the enum {@link Order}. The order may either be
+	 *            The order, which should be used to sort the items, as a value
+	 *            of the enum {@link Order}. The order may either be
 	 *            <code>ASCENDING</code> order <code>DESCENDING</code>
 	 */
 	void sort(Order order, Comparator<DataType> comparator);
 
 	/**
-	 * Adds a new listener, which should be notified when the adapter's
+	 * Adds a new listener, which should be notified, when the adapter's
 	 * underlying data has been sorted.
 	 * 
 	 * @param listener
@@ -57,8 +85,8 @@ public interface SortableListAdapter<DataType> {
 	void addSortingListner(final ListSortingListener<DataType> listener);
 
 	/**
-	 * Removes a specific listener, which should not be notified when the
-	 * adapter's underlying data has been modified, anymore.
+	 * Removes a specific listener, which should not be notified, when the
+	 * adapter's underlying data has been sorted, anymore.
 	 * 
 	 * @param listener
 	 *            The listener, which should be removed, as an instance of the
