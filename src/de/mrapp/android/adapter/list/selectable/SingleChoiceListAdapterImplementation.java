d6b99c39febae3442f70229637758f7d67dd6d0c
mrapp <mrapp@localhost>
2014-07-31 01:55:29 +0200

    Added comments.

--- src/de/mrapp/android/adapter/list/selectable/SingleChoiceListAdapterImplementation.java
+++ src/de/mrapp/android/adapter/list/selectable/SingleChoiceListAdapterImplementation.java
@@ -1,3 +1,20 @@
+/*
+ * AndroidAdapters Copyright 2014 Michael Rapp
+ *
+ * This program is free software: you can redistribute it and/or modify 
+ * it under the terms of the GNU Lesser General Public License as published 
+ * by the Free Software Foundation, either version 3 of the License, or 
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but 
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU 
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public 
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/>. 
+ */
 package de.mrapp.android.adapter.list.selectable;
 
 import java.util.ArrayList;
@@ -15,6 +32,19 @@ import de.mrapp.android.adapter.list.enablestate.ListEnableStateListener;
 import de.mrapp.android.adapter.list.itemstate.ListItemStateListener;
 import de.mrapp.android.adapter.list.sortable.ListSortingListener;
 
+/**
+ * An adapter, whose underlying data is managed as a list of arbitrary items, of
+ * which only item can be selected at once. Such an adapter's purpose is to
+ * provide the underlying data for visualization using a {@link ListView}
+ * widget.
+ * 
+ * @param <DataType>
+ *            The type of the adapter's underlying data
+ * 
+ * @author Michael Rapp
+ * 
+ * @since 1.0.0
+ */
 public class SingleChoiceListAdapterImplementation<DataType> extends
 		AbstractSelectableListAdapter<DataType> implements
 		SingleChoiceListAdapter<DataType> {
@@ -24,7 +54,15 @@ public class SingleChoiceListAdapterImplementation<DataType> extends
 	 */
 	private static final long serialVersionUID = 1L;
 
-	private ListAdapterListener<DataType> getAdapterListener() {
+	/**
+	 * Creates and returns a lister, which allows to adapt the selections of the
+	 * adapter's items, when an item has been removed from or added to the
+	 * adapter.
+	 * 
+	 * @return The listener, which has been created, as an instance of the type
+	 *         {@link ListAdapterListener}
+	 */
+	private ListAdapterListener<DataType> createAdapterListener() {
 		return new ListAdapterListener<DataType>() {
 
 			@Override
@@ -49,6 +87,57 @@ public class SingleChoiceListAdapterImplementation<DataType> extends
 		};
 	}
 
+	/**
+	 * Creates a new adapter, whose underlying data is managed as a list of
+	 * arbitrary items, of which only one item can be selected at once.
+	 * 
+	 * @param context
+	 *            The context, the adapter should belong to, as an instance of
+	 *            the class {@link Context}. The context may not be null
+	 * @param inflater
+	 *            The inflater, which should be used to inflate the views, which
+	 *            are used to visualize the adapter's items, as an instance of
+	 *            the type {@link Inflater}. The inflater may not be null
+	 * @param decorator
+	 *            The decorator, which should be used to customize the
+	 *            appearance of the views, which are used to visualize the items
+	 *            of the adapter, as an instance of the generic type
+	 *            DecoratorType. The decorator may not be null
+	 * @param items
+	 *            A list, which contains the the adapter's items, or an empty
+	 *            list, if the adapter should not contain any items
+	 * @param allowDuplicates
+	 *            True, if duplicate items should be allowed, false otherwise
+	 * @param adapterListeners
+	 *            A set, which contains the listeners, which should be notified
+	 *            when the adapter's underlying data has been modified or an
+	 *            empty set, if no listeners should be notified
+	 * @param enableStateListeners
+	 *            A set, which contains the listeners, which should be notified
+	 *            when an item has been disabled or enabled or an empty set, if
+	 *            no listeners should be notified
+	 * @param numberOfItemStates
+	 *            The number of states, the adapter's items may have, as an
+	 *            {@link Integer} value. The value must be at least 1
+	 * @param triggerItemStateOnClick
+	 *            True, if the state of an item should be triggered, when it is
+	 *            clicked by the user, false otherwise
+	 * @param itemStateListeners
+	 *            A set, which contains the listeners, which should be notified,
+	 *            when the state of an item has been changed or an empty set, if
+	 *            no listeners should be notified
+	 * @param sortingListeners
+	 *            A set, which contains the listeners, which should be notified,
+	 *            when the adapter's underlying data has been sorted or an empty
+	 *            set, if no listeners should be notified
+	 * @param selectItemOnClick
+	 *            True, if an item should be selected, when it is clicked by the
+	 *            user, false otherwise
+	 * @param selectionListeners
+	 *            A set, which contains the listeners, which should be notified,
+	 *            when an item's selection has been changed or an empty set, if
+	 *            no listeners should be notified
+	 */
 	protected SingleChoiceListAdapterImplementation(final Context context,
 			final Inflater inflater,
 			final SelectableListDecorator<DataType> decorator,
@@ -65,9 +154,26 @@ public class SingleChoiceListAdapterImplementation<DataType> extends
 				adapterListeners, enableStateListeners, numberOfItemStates,
 				triggerItemStateOnClick, itemStateListeners, sortingListeners,
 				selectItemOnClick, selectionListeners);
-		addAdapterListener(getAdapterListener());
+		addAdapterListener(createAdapterListener());
 	}
 
+	/**
+	 * Creates a new adapter, whose underlying data is managed as a list of
+	 * arbitrary items, of which only one item can be selected at once.
+	 * 
+	 * @param context
+	 *            The context, the adapter should belong to, as an instance of
+	 *            the class {@link Context}. The context may not be null
+	 * @param inflater
+	 *            The inflater, which should be used to inflate the views, which
+	 *            are used to visualize the adapter's items, as an instance of
+	 *            the type {@link Inflater}. The inflater may not be null
+	 * @param decorator
+	 *            The decorator, which should be used to customize the
+	 *            appearance of the views, which are used to visualize the items
+	 *            of the adapter, as an instance of the generic type
+	 *            DecoratorType. The decorator may not be null
+	 */
 	public SingleChoiceListAdapterImplementation(final Context context,
 			final Inflater inflater,
 			final SelectableListDecorator<DataType> decorator) {
