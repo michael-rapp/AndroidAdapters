9b640d51690df8d6d96989cc93f70dd8fd9bd9c8
Michael Rapp <michael.rapp90@googlemail.com>
2014-08-01 13:33:17 +0200

    Added copyright information.

--- src/de/mrapp/android/adapter/list/selectable/MultipleChoiceListAdapterImplementation.java
+++ src/de/mrapp/android/adapter/list/selectable/MultipleChoiceListAdapterImplementation.java
@@ -1,3 +1,20 @@
+/*
+ * AndroidAdapters Copyright 2014 Michael Rapp
+ *
+ * This program is free software: you can redistribute it and/or modify 
+ * it under the terms of the GNU Lesser General Public License as published 
+ * by the Free Software Foundation, either version 3 of the License, or 
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but 
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU 
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public 
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/>. 
+ */
 package de.mrapp.android.adapter.list.selectable;
 
 import java.util.ArrayList;
@@ -6,7 +23,6 @@ import java.util.List;
 import java.util.Set;
 
 import android.content.Context;
-import de.mrapp.android.adapter.ListDecorator;
 import de.mrapp.android.adapter.MultipleChoiceListAdapter;
 import de.mrapp.android.adapter.SelectableListDecorator;
 import de.mrapp.android.adapter.datastructure.item.Item;
