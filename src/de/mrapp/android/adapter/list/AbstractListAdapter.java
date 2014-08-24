209ccd9eebdc0bdcca071b52d115b3d790cff693
Michael Rapp <michael.rapp90@googlemail.com>
2014-08-25 01:58:57 +0200

    Removed unnecessary condition.

--- src/de/mrapp/android/adapter/list/AbstractListAdapter.java
+++ src/de/mrapp/android/adapter/list/AbstractListAdapter.java
@@ -418,7 +418,6 @@ public abstract class AbstractListAdapter<DataType, DecoratorType> extends
 
 	@Override
 	public final boolean addAllItems(final DataType... items) {
-		ensureNotNull(items, "The array may not be null");
 		return addAllItems(getNumberOfItems(), items);
 	}
 
