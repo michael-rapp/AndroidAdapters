93705239f24a2a4e67f55823eb91fba8aaf16664
mrapp <mrapp@localhost>
2014-08-29 17:32:10 +0200

    Added the interface SelectableExpandableListAdapter.

---
+++ src/de/mrapp/android/adapter/expandablelist/selectable/SelectableExpandableListAdapter.java
@@ -0,0 +1,8 @@
+package de.mrapp.android.adapter.expandablelist.selectable;
+
+import de.mrapp.android.adapter.ExpandableListAdapter;
+
+public interface SelectableExpandableListAdapter<GroupType, ChildType> extends
+		ExpandableListAdapter<GroupType, ChildType> {
+
+}
