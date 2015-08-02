cd87c068860db180e01dc8ffa1275e3d9f7b06e2
Michael Rapp <michael.rapp90@googlemail.com>
2015-08-02 22:16:32 +0200

    Edited comment.

--- src/de/mrapp/android/adapter/expandablelist/filterable/AbstractFilterableExpandableListAdapter.java
+++ src/de/mrapp/android/adapter/expandablelist/filterable/AbstractFilterableExpandableListAdapter.java
@@ -128,7 +128,7 @@ public abstract class AbstractFilterableExpandableListAdapter<GroupType, ChildTy
 	 *            an instance of the type {@link MultipleChoiceListAdapter}. The
 	 *            adapter may not be null
 	 * @param allowDuplicateChildren
-	 *            True, if duplicate group items, regardless from the group they
+	 *            True, if duplicate child items, regardless from the group they
 	 *            belong to, should be allowed, false otherwise
 	 * @param notifyOnChange
 	 *            True, if the method <code>notifyDataSetChanged():void</code>
@@ -140,22 +140,22 @@ public abstract class AbstractFilterableExpandableListAdapter<GroupType, ChildTy
 	 * @param itemClickListeners
 	 *            A set, which contains the listeners, which should be notified,
 	 *            when an item of the adapter has been clicked by the user, as
-	 *            an instance of the type {@link Set} or an empty set, if no
+	 *            an instance of the type {@link Set}, or an empty set, if no
 	 *            listeners should be notified
 	 * @param adapterListeners
 	 *            A set, which contains the listeners, which should be notified,
 	 *            when the adapter's underlying data has been modified, as an
-	 *            instance of the type {@link Set} or an empty set, if no
+	 *            instance of the type {@link Set}, or an empty set, if no
 	 *            listeners should be notified
 	 * @param expansionListeners
 	 *            A set, which contains the listeners, which should be notified,
 	 *            when a group item has been expanded or collapsed, as an
-	 *            instance of the type {@link Set} or an empty set, if no
+	 *            instance of the type {@link Set}, or an empty set, if no
 	 *            listeners should be notified
 	 * @param enableStateListeners
 	 *            A set, which contains the listeners, which should be notified,
 	 *            when an item has been disabled or enabled, as an instance of
-	 *            the type {@link Set} or an empty set, if no listeners should
+	 *            the type {@link Set}, or an empty set, if no listeners should
 	 *            be notified
 	 * @param numberOfGroupStates
 	 *            The number of states, the adapter's group items may have, as
@@ -171,12 +171,12 @@ public abstract class AbstractFilterableExpandableListAdapter<GroupType, ChildTy
 	 *            it is clicked by the user, false otherwise
 	 * @param itemStateListeners
 	 *            A set, which contains the listeners, which should be notified,
-	 *            when the state of an item has been changed or an empty set, if
-	 *            no listeners should be notified
+	 *            when the state of an item has been changed, or an empty set,
+	 *            if no listeners should be notified
 	 * @param sortingListeners
 	 *            A set, which contains the listeners, which should be notified,
-	 *            when the adapter's underlying data has been sorted or an empty
-	 *            set, if no listeners should be notified
+	 *            when the adapter's underlying data has been sorted, or an
+	 *            empty set, if no listeners should be notified
 	 * @param filterListeners
 	 *            A set, which contains the listeners, which should be notified,
 	 *            when the adapter's underlying data has been filtered, or an
