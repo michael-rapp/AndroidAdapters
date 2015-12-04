5cb787bea82cf99ead34d1fe6546b097edb627aa
Michael Rapp <michael.rapp90@googlemail.com>
2015-12-04 01:36:08 +0100

    Added factory methods, which allow to create adapters of the type MultipleChoiceExpandableListAdapter.

--- src/de/mrapp/android/adapter/AdapterFactory.java
+++ src/de/mrapp/android/adapter/AdapterFactory.java
@@ -20,6 +20,7 @@ package de.mrapp.android.adapter;
 import android.content.Context;
 import android.view.View;
 import de.mrapp.android.adapter.expandablelist.ExpandableListAdapterImplementation;
+import de.mrapp.android.adapter.expandablelist.selectable.MultipleChoiceExpandableListAdapterImplementation;
 import de.mrapp.android.adapter.expandablelist.selectable.SingleChoiceExpandableListAdapterImplementation;
 import de.mrapp.android.adapter.inflater.Inflater;
 import de.mrapp.android.adapter.inflater.InflaterFactory;
@@ -366,6 +367,92 @@ public final class AdapterFactory {
 		Inflater groupInflater = InflaterFactory.createInflater(groupView);
 		Inflater childInflater = InflaterFactory.createInflater(childView);
 		return new SingleChoiceExpandableListAdapterImplementation<>(context, groupInflater, childInflater, decorator,
+				choiceMode);
+	}
+
+	/**
+	 * Creates and returns an adapter, whose underlying data is managed as a
+	 * list of arbitrary group and child items, of which multiple items can be
+	 * selected at once.
+	 * 
+	 * @param <GroupType>
+	 *            The type of the underlying data of the adapter's group items
+	 * @param <ChildType>
+	 *            The type of the underlying data of the adapter's child items
+	 * @param context
+	 *            The context, the adapter belongs to, as an instance of the
+	 *            class {@link Context}. The context may not be null
+	 * @param decorator
+	 *            The The decorator, which should be used to customize the
+	 *            appearance of the views, which are used to visualize the group
+	 *            and child items of the adapter, as an instance of the type
+	 *            {@link SelectableExpandableListDecorator}. The decorator may
+	 *            not be null
+	 * @param groupViewId
+	 *            The resource id of the view, which should be used to visualize
+	 *            the group items of the adapter, as an {@link Integer} value.
+	 *            The id must correspond to a valid view resource
+	 * @param childViewId
+	 *            The resource id of the view, which should be used to visualize
+	 *            the child items of the adapter, as an {@link Integer} value.
+	 *            The id must correspond to a valid view resource
+	 * @param choiceMode
+	 *            The choice mode of the adapter as a value of the enum
+	 *            {@link ExpandableListChoiceMode}. The choice mode may either
+	 *            be <code>GROUPS_ONLY</code>, <code>CHILDREN_ONLY</code> or
+	 *            <code>GROUPS_AND_CHILDREN</code>
+	 * @return The adapter, which has been created, as an instance of the type
+	 *         {@link MultipleChoiceExpandableListAdapter}
+	 */
+	public static <GroupType, ChildType> MultipleChoiceExpandableListAdapter<GroupType, ChildType> createMultipleChoiceExpandableListAdapter(
+			final Context context, final SelectableExpandableListDecorator<GroupType, ChildType> decorator,
+			final int groupViewId, final int childViewId, final ExpandableListChoiceMode choiceMode) {
+		Inflater groupInflater = InflaterFactory.createInflater(groupViewId);
+		Inflater childInflater = InflaterFactory.createInflater(childViewId);
+		return new MultipleChoiceExpandableListAdapterImplementation<>(context, groupInflater, childInflater, decorator,
+				choiceMode);
+	}
+
+	/**
+	 * Creates and returns an adapter, whose underlying data is managed as a
+	 * list of arbitrary group and child items, of multiple items can be
+	 * selected at once.
+	 * 
+	 * @param <GroupType>
+	 *            The type of the underlying data of the adapter's group items
+	 * @param <ChildType>
+	 *            The type of the underlying data of the adapter's child items
+	 * @param context
+	 *            The context, the adapter belongs to, as an instance of the
+	 *            class {@link Context}. The context may not be null
+	 * @param decorator
+	 *            The The decorator, which should be used to customize the
+	 *            appearance of the views, which are used to visualize the group
+	 *            and child items of the adapter, as an instance of the type
+	 *            {@link SelectableExpandableListDecorator}. The decorator may
+	 *            not be null
+	 * @param groupView
+	 *            The view, which should be used to visualize the group items of
+	 *            the adapter, as an instance of the class {@link View}. The
+	 *            view may not be null
+	 * @param childView
+	 *            The view, which should be used to visualize the child items of
+	 *            the adapter, as an instance of the class {@link View}. The
+	 *            view may not be null
+	 * @param choiceMode
+	 *            The choice mode of the adapter as a value of the enum
+	 *            {@link ExpandableListChoiceMode}. The choice mode may either
+	 *            be <code>GROUPS_ONLY</code>, <code>CHILDREN_ONLY</code> or
+	 *            <code>GROUPS_AND_CHILDREN</code>
+	 * @return The adapter, which has been created, as an instance of the type
+	 *         {@link MultipleChoiceExpandableListAdapter}
+	 */
+	public static <GroupType, ChildType> MultipleChoiceExpandableListAdapter<GroupType, ChildType> createMultipleChoiceExpandableListAdapter(
+			final Context context, final SelectableExpandableListDecorator<GroupType, ChildType> decorator,
+			final View groupView, final View childView, final ExpandableListChoiceMode choiceMode) {
+		Inflater groupInflater = InflaterFactory.createInflater(groupView);
+		Inflater childInflater = InflaterFactory.createInflater(childView);
+		return new MultipleChoiceExpandableListAdapterImplementation<>(context, groupInflater, childInflater, decorator,
 				choiceMode);
 	}
 
